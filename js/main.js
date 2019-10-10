3e89b8d0bf6f4fa2e3d30363c83b067b97d07497
osamaaskar <osamaaskar0100@gmail.com>
2019-10-10 17:10:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(function(){
+    "use strict"
+    var winh = $(window).height();
+    var navh = $(".navbar").innerHeight();
+    
+    $(".header").height(winh);
+	
+	
+	
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
+	
+	
+	
+	
+	// ask and questions 
+	
+	
+	
+	
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+		
+	
+});
